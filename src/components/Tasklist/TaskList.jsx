b9d426d6227b792c913479b39fba93ddce80abde
AliHasan@2006 <alihasankazmi72@gmail.com>
2026-01-15 23:12:55 +0530

    TaskList created

---
+++ src/components/Tasklist/TaskList.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+
+const TaskList = () => {
+  return (
+    <div
+      id="taskList"
+      className="h-[55%] overflow-auto flex items-center justify-start gap-5 py-2 mt-10"
+    >
+      <div className={`shrink-0 h-full w-75 bg-green-400 rounded-xl`}>
+        <div className=" text-white p-5">
+          <div className="flex justify-between items-center text-sm font-medium">
+            <p className="bg-red-500 rounded-md text-sm py-2 px-4">High</p>
+            <p>26/11/2026</p>
+          </div>
+          <h2 className="text-2xl font-medium mt-4">Make a youtube video</h2>
+          <p className="text-gray-50">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit,
+            obcaecati.lorem5 Lorem ipsum dolor sit amet.
+          </p>
+        </div>
+      </div>
+      <div className={`shrink-0 h-full w-75 bg-blue-400 rounded-xl`}>
+        <div className=" text-white p-5">
+          <div className="flex justify-between items-center text-sm font-medium">
+            <p className="bg-red-500 rounded-md text-sm py-2 px-4">High</p>
+            <p>26/11/2026</p>
+          </div>
+          <h2 className="text-2xl font-medium mt-4">Make a youtube video</h2>
+          <p className="text-gray-50">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit,
+            obcaecati.lorem5 Lorem ipsum dolor sit amet.
+          </p>
+        </div>
+      </div>
+      <div className={`shrink-0 h-full w-75 bg-purple-400 rounded-xl`}>
+        <div className=" text-white p-5">
+          <div className="flex justify-between items-center text-sm font-medium">
+            <p className="bg-red-500 rounded-md text-sm py-2 px-4">High</p>
+            <p>26/11/2026</p>
+          </div>
+          <h2 className="text-2xl font-medium mt-4">Make a youtube video</h2>
+          <p className="text-gray-50">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit,
+            obcaecati.lorem5 Lorem ipsum dolor sit amet.
+          </p>
+        </div>
+      </div>
+      <div className={`shrink-0 h-full w-75 bg-yellow-400 rounded-xl`}>
+        <div className=" text-white p-5">
+          <div className="flex justify-between items-center text-sm font-medium">
+            <p className="bg-red-500 rounded-md text-sm py-2 px-4">High</p>
+            <p>26/11/2026</p>
+          </div>
+          <h2 className="text-2xl font-medium mt-4">Make a youtube video</h2>
+          <p className="text-gray-50">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit,
+            obcaecati.lorem5 Lorem ipsum dolor sit amet.
+          </p>
+        </div>
+      </div>
+      <div className={`shrink-0 h-full w-75 bg-blue-400 rounded-xl`}>
+        <div className=" text-white p-5">
+          <div className="flex justify-between items-center text-sm font-medium">
+            <p className="bg-red-500 rounded-md text-sm py-2 px-4">High</p>
+            <p>26/11/2026</p>
+          </div>
+          <h2 className="text-2xl font-medium mt-4">Make a youtube video</h2>
+          <p className="text-gray-50">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit,
+            obcaecati.lorem5 Lorem ipsum dolor sit amet.
+          </p>
+        </div>
+      </div>
+      <div className={`shrink-0 h-full w-75 bg-red-400 rounded-xl`}>
+        <div className=" text-white p-5">
+          <div className="flex justify-between items-center text-sm font-medium">
+            <p className="bg-red-500 rounded-md text-sm py-2 px-4">High</p>
+            <p>26/11/2026</p>
+          </div>
+          <h2 className="text-2xl font-medium mt-4">Make a youtube video</h2>
+          <p className="text-gray-50">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit,
+            obcaecati.lorem5 Lorem ipsum dolor sit amet.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TaskList;
