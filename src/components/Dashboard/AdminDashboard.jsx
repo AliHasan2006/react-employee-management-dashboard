2184b5b1546f26534153b79866405dc47fe9f317
AliHasan@2006 <alihasankazmi72@gmail.com>
2026-01-27 23:55:00 +0530

    Optimize Admin Dashboard

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Header from '../others/Header'
-import CreateTask from '../others/CreateTask'
-import AllTask from '../Tasklist/AllTask'
+import React from "react";
+import Header from "../others/Header";
+import CreateTask from "../others/CreateTask";
+import AllTask from "../Tasklist/AllTask";
 
-const AdminDashboard = () => {
+const AdminDashboard = ({data}) => {
   return (
-    <div className='p-10 h-screen w-screen text-white bg-[#121212]'>
-        <Header />
-        <CreateTask />
-        <AllTask />
+    <div className="p-10 h-screen w-screen text-white bg-[#121212]">
+      <Header data={data} />
+      <CreateTask data={data} />
+      <AllTask data={data} />
     </div>
-  )
-}
+  );
+};
 
-export default AdminDashboard
+export default AdminDashboard;
