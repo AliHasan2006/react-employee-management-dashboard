1856388d1b9be5e5b2e22ab3d198f64155e3036a
AliHasan@2006 <alihasankazmi72@gmail.com>
2026-03-02 22:42:55 +0530

    Minor changes in Header Component

--- src/components/others/Header.jsx
+++ src/components/others/Header.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 
 const Header = ({data}) => {
-//   console.log(data);
+  console.log(data);
 
-// const logOutUser = () => {
-//   localStorage.removeItem("loggedInUserData");
-//   setUserRole(null);
-//   setUserData(null);
-// };
+const logOutUser = () => {
+localStorage.removeItem("loggedInUser");
+window.location.reload();
+};
   return (
     <div className="text-white bg-[#121212] flex justify-between items-end">
       <h1 className="text-2xl font-medium">
-        Hello, <br />
+        Hello, {data.firstName}<br />
         <span className="text-3xl font-semibold">👋</span>
       </h1>
-      <button className="bg-red-500 hover:bg-red-400 out-none text-white px-5 py-2 rounded-sm text-lg font-medium cursor-pointer">
+      <button onClick={logOutUser} className="bg-red-500 hover:bg-red-400 out-none text-white px-5 py-2 rounded-sm text-lg font-medium cursor-pointer">
         Log out
       </button>
     </div>
